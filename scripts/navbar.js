40beb0eaa353a0f2125d86d89d621da96ee91a57
dascolt1 <dascolt1@tcnj.edu>
2021-02-22 15:31:53 -0500

    no more headaches

---
+++ scripts/navbar.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', function (e) {
+    let top = this.scrollY;
+    let nav = document.querySelector('.navbar');
+
+    if(top > 70){
+        nav.style.opacity = '.85';
+    }
+
+    if(top < 70) {
+        nav.style.opacity = '1';
+    }
+    
+})
+
+console.log("working")
