2fbdb50ec0978a89f4a480687dfddca3fcf1c426
dascolt1 <dascolt1@tcnj.edu>
2021-02-22 18:38:37 -0500

    opacity transition

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,11 +2,12 @@ window.addEventListener('scroll', function (e) {
     let top = this.scrollY;
     let nav = document.querySelector('.navbar');
 
-    if(top > 70){
+    if(top > 60){
+        nav.style.transition = 'opacity 800ms'; 
         nav.style.opacity = '.7';
     }
 
-    if(top < 70) {
+    if(top < 60) {
         nav.style.opacity = '1';
     }
     
